607337780ee8fab6a189932ba9066111f993372b
joancliville <109911945+joancliville@users.noreply.github.com>
2023-03-11 16:27:23 +0100

    Update martin_v1.py

--- round1/martin_v1.py
+++ round1/martin_v1.py
@@ -2,38 +2,28 @@ import pandas as pd
 import numpy as np
 from scipy.stats import norm
 
-# Definim parametres per Avellaneda Stoikov
+def calculate_optimal_spread(data, gamma=1, alpha=0.1):
+    if data.empty:
+        raise ValueError("Data cannot be empty.")
 
-gamma = 1
-alpha = 0.1
-n_sigma = 2
-
-def calculate_optimal_spread(data):
-    # Calculem vwap i stdev
     vwap = (data['price'] * data['volume']).sum() / data['volume'].sum()
     std_dev = data['price'].std()
-
-    # calcul optimal bid-ask
     spread = 2 * alpha * std_dev / (gamma * vwap)
-
     return spread
 
-def place_orders(data, open_lots, spread):
-    # Calculem bid i ask prices
+def place_orders(data, open_lots, spread, gamma=1, n_sigma=2):
+    if data.empty:
+        raise ValueError("Data cannot be empty.")
+
     vwap = (data['price'] * data['volume']).sum() / data['volume'].sum()
     mid_price = vwap + spread / 2
     bid_price = mid_price - spread / 2
     ask_price = mid_price + spread / 2
 
-    # calculem quanitat optima
     quantity = gamma * (mid_price - data['price'].iloc[-1]) / spread
-
-    # Probabilitat de que una ordre sigui filled
     prob_fill = norm.cdf(n_sigma * np.abs(quantity))
 
-    # Posa ordres
     if open_lots == 0:
-        # no lots open
         if np.random.rand() < prob_fill:
             if quantity > 0:
                 order = {'price': ask_price, 'lots': quantity}
@@ -42,12 +32,9 @@ def place_orders(data, open_lots, spread):
         else:
             order = None
     else:
-        # tancar o no lots oberts
         if np.sign(open_lots) == np.sign(quantity):
-            # mateixa direccio, hold posicio
             order = None
         elif np.abs(open_lots) > np.abs(quantity):
-            # tanca part profitable de la posicio
             if np.random.rand() < prob_fill:
                 if quantity > 0:
                     order = {'price': ask_price, 'lots': -np.sign(open_lots) * np.abs(quantity)}
@@ -56,7 +43,6 @@ def place_orders(data, open_lots, spread):
             else:
                 order = None
         else:
-            # tanca posicio sencera
             if np.random.rand() < prob_fill:
                 if quantity > 0:
                     order = {'price': ask_price, 'lots': -open_lots}
